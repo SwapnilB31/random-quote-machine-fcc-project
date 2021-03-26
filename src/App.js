7a3932ac4d1314126fc33464782ff64c78e1d74a
SwapnilB31 <swapnilbhattacharjee187@gmail.com>
2021-03-27 00:58:34 +0530

    added files for the first time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import './fonts.css';
+import quotes from './quotes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, Button} from 'react-bootstrap';
+import {FaTwitter} from 'react-icons/fa'
 
 function App() {
+  const [index,setIndex] = useState(Math.floor(Math.random() * 60 + 1));
+  const [quote,setQuote] = useState('');
+  const [author,setAuthor] = useState('');
+  const [tweetLink,setTweetLink] = useState('');
+
+  useEffect(()=> {
+    setQuote(quotes[index].quote);
+    setAuthor(quotes[index].by);
+    const twtLink = `https://twitter.com/intent/tweet?text=${encodeURI(quotes[index].quote)} - ${encodeURI(quotes[index].by)}`;
+    setTweetLink(twtLink);
+  },[index])
+
+  function newQuote() {
+    const indexNum = Math.random() * 60 + 1;
+    setIndex(Math.floor(indexNum))
+    console.log(index);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-body">
+      <Card style={{width : '70vw'}}>
+        <div id="quote-box" className="pad-a-little">
+          <Card.Title>
+            <blockquote className="quote" id="text">{quote}</blockquote>
+          </Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            <div className="author" id="author">{author}</div>
+          </Card.Subtitle>
+          
+          {/*<button id="new-quote" onClick={newQuote}>New Quote</button>*/}
+          <hr/>
+          <div className="button-panel">
+            <Button  variant="info" id="new-quote" onClick={newQuote}>New Quote</Button>
+            <a className="btn btn-info" target='_blank' rel='noreferrer' href={tweetLink} id="tweet-quote"><FaTwitter/></a>
+          </div>
+        </div>
+      </Card>
     </div>
+    
   );
 }
 
---
+++ src/quotes.js
@@ -0,0 +1,248 @@
+const quotes =  {
+	1 : {
+		"quote" : "You can do anything, but not everything.",
+		"by" : "David Allen"
+	},
+	2 : {
+		"quote" : "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.",
+		"by" : "Antoine de Saint-Exupéry"
+	},
+	3 : {
+		"quote" : "The richest man is not he who has the most, but he who needs the least.",
+		"by" : "Unknown Author"
+	},
+	4 : {
+		"quote" : "You miss 100 percent of the shots you never take.",
+		"by" : "Wayne Gretzky"
+	},
+	5 : {
+		"quote" : "You must be the change you wish to see in the world",
+		"by" : "Gandhi"
+	},
+	6 : {
+		"quote" : "When hungry, eat your rice; when tired, close your eyes. Fools may laugh at me, but wise men will know what I mean.",
+		"by" : "Lin-Chi"
+	},
+	7 : {
+		"quote" : "Courage is not the absence of fear, but rather the judgement that something else is more important than fear.",
+		"by" : "Ambrose Redmoon"
+	},
+	8 : {
+		"quote" : "The third-rate mind is only happy when it is thinking with the majority. The second-rate mind is only happy when it is thinking with the minority. The first-rate mind is only happy when it is thinking.",
+		"by" : "A. A. Milne"
+	},
+	9 : {
+		"quote" : "To the man who only has a hammer, everything he encounters begins to look like a nail.",
+		"by" : "Abraham Maslow"
+	},
+	10 : {
+		"quote" : "We are what we repeatedly do; excellence, then, is not an act but a habit.",
+		"by" : "Aristotle"
+	},
+	11 : {
+		"quote" : "A wise man gets more use from his enemies than a fool from his friends.",
+		"by" : "Baltasar Gracian"
+	},
+	12 : {
+		"quote" : "Do not seek to follow in the footsteps of the men of old; seek what they sought.",
+		"by" : "Basho"
+	},
+	13 : {
+		"quote" : "Watch your thoughts; they become words. Watch your words; they become actions.Watch your actions; they become habits.Watch your habits; they become character.Watch your character; it becomes your destiny.",
+		"by" : "Lao-Tze"
+	},
+	14 : {
+		"quote" : "Everyone is a genius at least once a year. The real geniuses simply have their bright ideas closer together.",
+		"by" : "Georg Christoph Lichtenberg"
+	},
+	15 : {
+		"quote" : "What we think, or what we know, or what we believe is, in the end, of little consequence. The only consequence is what we do.",
+		"by" : "John Ruskin"
+	},
+	16 : {
+		"quote" : "The real voyage of discovery consists not in seeking new lands but seeing with new eyes.",
+		"by" : "Marcel Proust"
+	},
+	17 : {
+		"quote" : "Work like you don’t need money, love like you’ve never been hurt, and dance like no one’s watching.",
+		"by" : "Unknown Author"
+	},
+	18 : {
+		"quote" : "Try a thing you haven’t done three times. Once, to get over the fear of doing it. Twice, to learn how to do it. And a third time, to figure out whether you like it or not.",
+		"by" : "Virgil Garnett Thomson"
+	},
+	19 : {
+		"quote" : "Even if you’re on the right track, you’ll get run over if you just sit there.",
+		"by" : "Will Rogers"
+	},
+	20 : {
+		"quote" : "People often say that motivation doesn’t last. Well, neither does bathing – that’s why we recommend it daily.",
+		"by" : "Zig Ziglar"
+	},
+	21 : {
+		"quote" : "Before I got married I had six theories about bringing up children; now I have six children and no theories.",
+		"by" : "John Wilmot"
+	},
+	22 : {
+		"quote" : "What the world needs is more geniuses with humility, there are so few of us left.",
+		"by" : "Oscar Levant"
+	},
+	23 : {
+		"quote" : "Always forgive your enemies; nothing annoys them so much",
+		"by" : "Oscar Wilde"
+	},
+	24 : {
+		"quote" : "I’ve gone into hundreds of [fortune-teller’s parlors], and have been told thousands of things, but nobody ever told me I was a policewoman getting ready to arrest her.",
+		"by" : "New York City Detective"
+	},
+	25 : {
+		"quote" : "When you go into court you are putting your fate into the hands of twelve people who weren’t smart enough to get out of jury duty.",
+		"by" : "Norm Crosby"
+	},
+	26 : {
+		"quote" : "Those who believe in telekinetics, raise my hand.",
+		"by" : "Kurt Vonnegut"
+	},
+	27 : {
+		"quote" : "Just the fact that some geniuses were laughed at does not imply that all who are laughed at are geniuses. They laughed at Columbus, they laughed at Fulton, they laughed at the Wright brothers. But they also laughed at Bozo the Clown.",
+		"by" : "Carl Sagan"
+	},
+	28 : {
+		"quote" : "My pessimism extends to the point of even suspecting the sincerity of the pessimists.",
+		"by" : "Jean Rostand"
+	},
+	29 : {
+		"quote" : "Sometimes I worry about being a success in a mediocre world.",
+		"by" : "Lily Tomlin"
+	},
+	30 : {
+		"quote" : "I quit therapy because my analyst was trying to help me behind my back.",
+		"by" : "Richard Lewis"
+	},
+	31 : {
+		"quote" : "We’ve heard that a million monkeys at a million keyboards could produce the complete works of Shakespeare; now, thanks to the Internet, we know that is not true.",
+		"by" : "Robert Wilensky"
+	},
+	32 : {
+		"quote" : "If there are no stupid questions, then what kind of questions do stupid people ask? Do they get smart just in time to ask questions?",
+		"by" : "Scott Adams"
+	},
+	33 : {
+		"quote" : "If the lessons of history teach us anything it is that nobody learns the lessons that history teaches us.",
+		"by" : "The Annonymous Writer"
+	},
+	34 : {
+		"quote" : "When I was a boy I was told that anybody could become President. Now I’m beginning to believe it.",
+		"by" : "Clarence Darrow"
+	},
+	35 : {
+		"quote" : "Laughing at our mistakes can lengthen our own life. Laughing at someone else’s can shorten it.",
+		"by" : "Cullen Hightower"
+	},
+	36 : {
+		"quote" : "There are many who dare not kill themselves for fear of what the neighbors will say.",
+		"by" : "Cyril Connolly"
+	},
+	37 : {
+		"quote" : "There’s so much comedy on television. Does that cause comedy in the streets?",
+		"by" : "Dick Cavett"
+	},
+	38 : {
+		"quote" : "All men are frauds. The only difference between them is that some admit it. I myself deny it.",
+		"by" : "H. L. Mencken"
+	},
+	39 : {
+		"quote" : "I don’t mind what Congress does, as long as they don’t do it in the streets and frighten the horses.",
+		"by" : "Victor Hugo"
+	},
+	40 : {
+		"quote" : "I took a speed reading course and read ‘War and Peace’ in twenty minutes. It involves Russia.",
+		"by" : "Woody Allen"
+	},
+	41 : {
+		"quote" : "The person who reads too much and uses his brain too little will fall into lazy habits of thinking.",
+		"by" : "Albert Einstien"
+	},
+	42 : {
+		"quote" : "Believe those who are seeking the truth. Doubt those who find it.",
+		"by" : "André Gide"
+	},
+	43 : {
+		"quote" : "It is the mark of an educated mind to be able to entertain a thought without accepting it.",
+		"by" : "Aristotle"
+	},
+	44 : {
+		"quote" : "I’d rather live with a good question than a bad answer.",
+		"by" : "Aryeh Frimer"
+	},
+	45 : {
+		"quote" : "We learn something every day, and lots of times it’s that what we learned the day before was wrong.",
+		"by" : "Bill Vaughan"
+	},
+	46 : {
+		"quote" : "I have made this letter longer than usual because I lack the time to make it shorter.",
+		"by" : "Blaise Pascal"
+	},
+	47 : {
+		"quote" : "Don’t ever wrestle with a pig. You’ll both get dirty, but the pig will enjoy it.",
+		"by" : "Cale Yarborough",
+	},
+	48 : {
+		"quote" : "An inventor is simply a fellow who doesn’t take his education too seriously.",
+		"by" : "Charles F. Kettering"
+	},
+	49 : {
+		"quote" : "Asking a working writer what he thinks about critics is like asking a lamppost how it feels about dogs.",
+		"by" : "Christopher Hampton"
+	},
+	50 : {
+		"quote" : "Better to write for yourself and have no public, than to write for the public and have no self.",
+		"by" : "Cyril Connolly"
+	},
+	51 : {
+		"quote" : "Never be afraid to laugh at yourself, after all, you could be missing out on the joke of the century.",
+		"by" : "Dame Edna Everage"
+	},
+	52 : {
+		"quote" : "I am patient with stupidity but not with those who are proud of it.",
+		"by" : "Edith Sitwell"
+	},
+	53 : {
+		"quote" : "Normal is getting dressed in clothes that you buy for work and driving through traffic in a car that you are still paying for – in order to get to the job you need to pay for the clothes and the car, and the house you leave vacant all day so you can afford to live in it.",
+		"by" : "Ellen Goodman"
+	},
+	54 : {
+		"quote" : "The cure for boredom is curiosity. There is no cure for curiosity.",
+		"by" : "Ellen Parr"
+	},
+	55 : {
+		"quote" : "Advice is what we ask for when we already know the answer but wish we didn’t.",
+		"by" : "Erica Jong"
+	},
+	56 : {
+		"quote" : "Some people like my advice so much that they frame it upon the wall instead of using it.",
+		"by" : "Gordon R. Dickson"
+	},
+	57 : {
+		"quote" : "The trouble with the rat race is that even if you win, you’re still a rat.",
+		"by" : "Lily Tomlin"
+	},
+	58 : {
+		"quote" : "Never ascribe to malice, that which can be explained by incompetence.",
+		"by" : "Napoleon"
+	},
+	59 : {
+		"quote" : "Imagination was given to man to compensate him for what he is not, and a sense of humor was provided to console him for what he is.",
+		"by" : "Oscar Wilde"
+	},
+	60 : {
+		"quote" : "When a person can no longer laugh at himself, it is time for others to laugh at him.",
+		"by" : "Thomas Szasz"
+	},
+  61 : {
+    "quote" : "The universe, which you are the center of, is expanding. And eventually, everyone around you is just going to drift away.",
+    "by" : "Ajas Mohammed"
+  }
+};
+
+export default quotes;
